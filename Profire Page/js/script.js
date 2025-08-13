be5a927cba57ab6c58c7538534c32796864d8d71
thisismammad <thisismamamd7@gmail.com>
2025-08-13 14:09:45 +0330

    add bamburger menu to profile project

--- Profire Page/js/script.js
+++ Profire Page/js/script.js
@@ -3,11 +3,13 @@ const $ = document;
 const darkmodeBtn = $.querySelector("#dark-mode");
 const root = $.documentElement;
 const header = $.querySelector("header");
+const menuBtn = $.querySelector(".menu-btn");
+const menu = $.querySelector(".menu");
 
 darkmodeBtn.addEventListener("click", () => {
 	changeMode();
 });
-
+let isMenuOpen = false;
 let isDark = false;
 function changeMode() {
 	if (isDark) {
@@ -18,8 +20,7 @@ function changeMode() {
 		root.style.setProperty("--light-gray", "#f6f6f6");
 		root.style.setProperty("--primary-white", "#ffffff");
 		root.style.setProperty("--shadow", "#c6c6c6");
-    header.style.backgroundImage = "url(./images/cover.jpg)";
-
+		header.style.backgroundImage = "url(./images/cover.jpg)";
 	} else {
 		darkmodeBtn.innerHTML = "<i class='fa-solid fa-sun'></i>";
 		root.style.setProperty("--primary-black", "#ffffff");
@@ -27,7 +28,24 @@ function changeMode() {
 		root.style.setProperty("--light-gray", "#242424");
 		root.style.setProperty("--primary-white", "#2f3640");
 		root.style.setProperty("--shadow", "#242424");
-    header.style.backgroundImage = "url(./images/cover2.jpg)";
+		header.style.backgroundImage = "url(./images/cover2.jpg)";
 		isDark = true;
 	}
 }
+
+menuBtn.addEventListener("click", () => {
+	if (isMenuOpen) {
+		menu.style.height = "0";
+		isMenuOpen = false;
+	} else {
+		menu.style.height = "185px";
+		isMenuOpen = true;
+	}
+});
+
+window.addEventListener("resize", () => {
+	if (isMenuOpen) {
+		menu.style.height = "0";
+		isMenuOpen = false;
+	}
+});
