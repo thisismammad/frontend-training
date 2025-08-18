951d19f9cb7d696cd7b64857372a2436202980b1
thisismammad <thisismamamd7@gmail.com>
2025-08-18 17:57:23 +0330

    async await

---
+++ javascript/test/async-await.js
@@ -0,0 +1,16 @@
+// async- await
+
+async function sendResponse() {
+	try {
+		const response = await fetch(
+			"https://jsonplaceholder.typicode.com/todos/1"
+		);
+		console.log(response);
+		const data = await response.json();
+		console.log(data);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+sendResponse();
