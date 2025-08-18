c0b6e3c55e62bb946c61a1f91ad69d70e98ba801
thisismammad <thisismamamd7@gmail.com>
2025-08-18 17:46:48 +0330

    Promise

--- javascript/test/script.js
+++ javascript/test/script.js
@@ -1,12 +1,30 @@
-// callback
+// promises
 
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+// 	.then((response) => {
+// 		return response.json();
+// 	})
+// 	.then((data) => {
+// 		console.log(data);
+// 	});
 
-
-function showInfo(name, price,callback){
-  const result = `name:${name} - price:${price}`;
-  callback(result);
-}
-
-showInfo("apple", 25,(res)=>{
-  console.log(res);
+const prom = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve("Done");
+	}, 3000);
 });
+
+prom.then((data) => {
+	console.log(data);
+});
+
+fetch("https://jsonplaceholder.typicode.cm/todos/1")
+	.then((response) => {
+		return response.json();
+	})
+	.catch((err) => {
+		console.log(err); 
+	})
+	.then((data) => {
+		console.log(data);
+	});
