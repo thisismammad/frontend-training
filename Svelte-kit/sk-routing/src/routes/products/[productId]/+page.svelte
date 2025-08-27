a76a8bc720f79c02bc504383c838bc3e89b2947b
thisismammad <thisismamamd7@gmail.com>
2025-08-27 14:40:05 +0330

    sveltekit Dyanamic Routes

---
+++ Svelte-kit/sk-routing/src/routes/products/[productId]/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { page } from "$app/stores";
+  const productId = $page.params.productId;
+</script>
+
+<h1>Details about product: {productId}</h1>
+
+<style>
+  h1 {
+    background-color: slategray;
+    font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+      "Lucida Sans", Arial, sans-serif;
+    padding: 2rem;
+    border-radius: 0.5rem;
+  }
+</style>
