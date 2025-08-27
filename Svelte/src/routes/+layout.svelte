c23e2116595126b09ebbe39b0f90684d6e169977
thisismammad <thisismamamd7@gmail.com>
2025-08-27 13:59:21 +0330

    sveltekit hello world

---
+++ Svelte/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+let name:string = "mammad"
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+<h1>{name.toUpperCase()}</h1>
+
+{@render children?.()}
