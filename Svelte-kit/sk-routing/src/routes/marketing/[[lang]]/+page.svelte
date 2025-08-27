15262c2bd4363296d561fc8c8ce62918a99f94d5
thisismammad <thisismamamd7@gmail.com>
2025-08-27 22:43:11 +0330

    sveltekit optional parameters

---
+++ Svelte-kit/sk-routing/src/routes/marketing/[[lang]]/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  interface Langs {
+    [key: string]: string;
+  }
+  const greetings: Langs = {
+    en: "hello!",
+    es: "Hola!",
+    fa: "!سلام",
+  };
+
+  const lang = $page.params.lang || "en";
+</script>
+
+{#if typeof lang === "string"}
+  <h1>{greetings[lang]}</h1>
+{/if}
