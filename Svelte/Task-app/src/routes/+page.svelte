2eb67f37cf997e556d28cdde10803bd02cc06820
thisismammad <thisismamamd7@gmail.com>
2025-09-06 23:51:59 +0330

    first Svelte project Task App: some tailwind component and base styles

--- Svelte/Task-app/src/routes/+page.svelte
+++ Svelte/Task-app/src/routes/+page.svelte
@@ -1,1 +1,23 @@
-<h1>e</h1>
+<input type="text" placeholder="new ToDo" />
+
+<button class=" button bg-blue-900 hover:bg-blue-800">Add</button>
+<button class=" button taps active">all</button>
+<button class=" button delete">done</button>
+<button class=" button">todo</button>
+
+<ul class="list">
+	<li class="list-item">
+		<label>
+			<input type="checkbox" name="" id="" />
+			Item 1
+		</label>
+		<button class=" button delete">Remove</button>
+	</li>
+	<li class="list-item">
+		<label class="done">
+			<input type="checkbox" name="" id="" />
+			Item 2
+		</label>
+		<button class=" button delete">Remove</button>
+	</li>
+</ul>
